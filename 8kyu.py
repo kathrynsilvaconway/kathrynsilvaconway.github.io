9d998762ed8142d5d8166024b75948c756f5e7bf
Kathryn Silva <kathrynsilvaconway@yahoo.com>
2021-12-17 23:07:26 -0500

    re-did the regular ball/super ball one. gained a little more clarity around oop

--- 8kyu.py
+++ 8kyu.py
@@ -96,3 +96,12 @@
 
 # def reverse_list(l):
 #     return list(reversed(l))
+class Ball(object):
+
+    def __init__(self, ball_type = "regular"):
+        self.ball_type = ball_type
+
+ball1 = Ball()
+ball2 = Ball("super")
+print(ball1.ball_type)  #=> "regular"
+ball2.ball_type  #=> "super"
