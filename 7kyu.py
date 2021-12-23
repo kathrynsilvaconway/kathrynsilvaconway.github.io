c478d7a6210789373215d3e8ad0ff08e0a16990f
Kathryn Silva <kathrynsilvaconway@yahoo.com>
2021-12-23 16:10:18 -0500

    The two oldest kata was actually very easy. I thought the sort method was removing my duplicates, but it actually does not. I find I often have the problem solved, but am held back by a small detail and I convince myself that I have it all wrong. Also worked out a simple OOP Kata. Need a lot more understanding in this area.

--- 7kyu.py
+++ 7kyu.py
@@ -28,7 +28,17 @@
 
 # print(compare("!!", "7476"))
 # def two_oldest_ages(ages):
-#     oldest = []
-#     oldest.append(list(set(ages))[-2])
-#     oldest.append(list(set(ages))[-1])
-#     return oldest
+#     return sorted(ages[:-2])
+# print(two_oldest_ages([84, 48, 27, 63, 25, 67, 90, 25, 63, 76, 69, 58, 86, 58, 0, 86, 23, 90, 81]))
+# # print(two_oldest_ages([1, 5, 87, 45, 8, 8])
+class Person:
+    
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self, your_name):
+        return f"Hello {your_name}, my name is {self.name}"
+
+jack = Person("Jack")
+jill = Person("Jill")
+print(jack.greet("jill"))
