dfea1a1c84b992a0f70f72d9d3716b61862b7bf2
Kathryn Silva <kathrynsilvaconway@yahoo.com>
2021-12-27 21:56:27 -0500

    no real changes today. Resited a few and found they were super easy.

--- 7kyu.py
+++ 7kyu.py
@@ -31,14 +31,31 @@
 #     return sorted(ages[:-2])
 # print(two_oldest_ages([84, 48, 27, 63, 25, 67, 90, 25, 63, 76, 69, 58, 86, 58, 0, 86, 23, 90, 81]))
 # # print(two_oldest_ages([1, 5, 87, 45, 8, 8])
-class Person:
+# class Person:
     
-    def __init__(self, name):
-        self.name = name
+#     def __init__(self, name):
+#         self.name = name
 
-    def greet(self, your_name):
-        return f"Hello {your_name}, my name is {self.name}"
+#     def greet(self, your_name):
+#         return f"Hello {your_name}, my name is {self.name}"
 
-jack = Person("Jack")
-jill = Person("Jill")
-print(jack.greet("jill"))
+# jack = Person("Jack")
+# jill = Person("Jill")
+# print(jack.greet("jill"))
+
+# class Person():
+
+#     def __init__(self, first_name, last_name, age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.age = age
+#         self.full_name = first_name + ' '+last_name
+
+# return a float round to two decimal places:
+# def solution(n):
+#     return float(format(n, ".2f"))
+# Easier:
+# def solution(n):
+#     return round(n, 2)
+
+
