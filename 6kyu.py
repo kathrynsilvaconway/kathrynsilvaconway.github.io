07d56a1b2f29c15ff5eefa79b763f132c42b9142
Kathryn Silva <kathrynsilvaconway@yahoo.com>
2022-02-03 21:17:21 -0500

    difficulty with index out of range because of neg indexing." exit

--- 6kyu.py
+++ 6kyu.py
@@ -1,15 +1,48 @@
-def cake(candles,debris):
-    total = 0
-    alpha_position = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26}
-    # debris = list(debris)
-    for x in range(len(debris)):
-        if x % 2 == 0:
-            total += ord(debris[x])
-            # print(f'current even total: {ord(debris[x])}')
-        else:
-            if debris[x] in alpha_position:
-                total += alpha_position[debris[x]]
-                # print(f'current odd total: {alpha_position[debris[x]]}')
-    return "Fire!" if (candles * .7) < total else "That was close!"
+# def cake(candles,debris):
+#     total = 0
+#     alpha_position = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26}
+#     # debris = list(debris)
+#     for x in range(len(debris)):
+#         if x % 2 == 0:
+#             total += ord(debris[x])
+#             # print(f'current even total: {ord(debris[x])}')
+#         else:
+#             if debris[x] in alpha_position:
+#                 total += alpha_position[debris[x]]
+#                 # print(f'current odd total: {alpha_position[debris[x]]}')
+#     return "Fire!" if (candles * .7) < total else "That was close!"
 
-print(cake(12, 'jaam'))
+# print(cake(400, 'jaam'))
+
+def compare(s1,s2):
+    count_s1, count_s2 = 0, 0
+    allowed_chars ='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    print(type(s1))
+    if s1 != None:
+        for x in s1.upper():
+            if x not in allowed_chars or s1 =='':
+                count_s1 = 0
+            else:
+                count_s1 += ord(x)
+    if s2 != None:
+        for x in s2.upper():
+            if x not in allowed_chars or s2 =='':
+                count_s2 = 0
+            else:
+                count_s2 += ord(x)
+            
+            
+    return True if count_s1 == count_s2 else False
+
+print(compare('zz1', ''))
+
+# def two_oldest_ages(ages):
+#     ages2 = []
+#     for x in ages:
+#         if x not in ages2:
+#             ages2.append(x)
+#     ages2.sort()
+#     print(ages2)
+#     result = [ages2[-2], ages2[-1]]
+#     return result
+# print(two_oldest_ages([1, 5, 87, 45, 8, 8]))
